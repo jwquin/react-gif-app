e95d4f4a4367886a023c9d5c61d4efa202c5ad7b
jquin <jorgequin189@hotmail.com>
2021-05-19 17:22:08 -0500

    Primer commit

---
+++ src/components/GifGridItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const GifGridItem = ({url, title}) => {
+      
+    return (
+        <div className = 'card animate__animated animate__fadeInLeft'>
+        
+            <img src={url} alt={title}/>
+            <p>{title}</p>                       
+        </div>
+    )
+}
+
+
